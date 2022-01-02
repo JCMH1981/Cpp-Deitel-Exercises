3711de8e8cc049ad75b865c7ff1477769f10837c
JCMH1981 <jcmhsoftware@gmail.com>
2022-01-01 19:23:27 -0500

    File README.md and exercise 2-16.cpp

---
+++ Chapter-2/2-16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+
+	cout << "Enter the first number: ";
+	cin >> a;
+	cout << "Enter the second number: ";
+	cin >> b;
+
+	cout << "Sum is " << a + b << endl;
+	cout << "Product is " << a * b << endl;
+	cout << "Difference is " << a - b << endl;
+	cout << "Quotient is " << a / b << endl;
+	
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Cpp-Deitel-Exercises
+
+Solved exercises from the book C++ How To Program, 9/E, by
+Harvey Deitel & Paul Deitel.
