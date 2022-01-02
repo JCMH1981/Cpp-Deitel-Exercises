ebce2bdc9401a4f66b9fb11a7d79bde107d66651
JCMH1981 <jcmhsoftware@gmail.com>
2022-01-01 20:23:58 -0500

    Added description to 2-16.cpp

--- Chapter-2/2-16.cpp
+++ Chapter-2/2-16.cpp
@@ -1,3 +1,9 @@
+/* C++ How to Program, 9/E, by Paul Deitel & Harvey Deitel.
+Solution of exercise 2.16:
+(Arithmetic) Write a program that asks the user to enter two numbers, obtains 
+the two numbers from the user and prints the sum, product, difference, and 
+quotient of the two numbers. */
+/* Written by Juan Carlos Moreno, 2022/01/01 */
 #include <iostream>
 
 using namespace std;
