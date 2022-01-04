17513dd40d00142f06c85aa01e12e01ddb24581a
JCMH1981 <jcmhsoftware@gmail.com>
2022-01-03 20:45:22 -0500

    Added exercise 2.21

---
+++ Chapter-2/2-21.cpp
@@ -0,0 +1,26 @@
+/* C++ How to Program, 9/E, by Paul Deitel & Harvey Deitel.
+
+Solution of exercise 2.21:
+(Displaying Shapes with Asterisks) Write a program that prints a box, an oval, 
+an arrow and a diamond as follows:
+
+Written by Juan Carlos Moreno (jcmhsoftware@gmail.com), 2022/01/03 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "*********          ***            *            *" << endl;
+	cout << "*       *        *     *         ***          * *" << endl;
+	cout << "*       *       *       *       *****        *   *" << endl;
+	cout << "*       *       *       *         *         *     *" << endl;
+	cout << "*       *       *       *         *        *       *" << endl;
+	cout << "*       *       *       *         *         *     *" << endl;
+	cout << "*       *       *       *         *          *   *" << endl;
+	cout << "*       *        *     *          *           * *" << endl;
+	cout << "*********          ***            *            *" << endl;
+
+	return 0;
+}
