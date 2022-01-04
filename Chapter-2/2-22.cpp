32985f869ccf1258185d9a86014d2ad87fd555f7
JCMH1981 <jcmhsoftware@gmail.com>
2022-01-03 22:39:58 -0500

    Added exercise 2.22

---
+++ Chapter-2/2-22.cpp
@@ -0,0 +1,17 @@
+/* C++ How to Program, 9/E, by Paul Deitel & Harvey Deitel.
+
+Solution of exercise 2.22:
+What does the following code print?
+cout << "*\n**\n***\n****\n*****" << endl;
+
+
+Written by Juan Carlos Moreno (jcmhsoftware@gmail.com), 2022/01/03 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "*\n**\n***\n****\n*****" << endl;
+}
